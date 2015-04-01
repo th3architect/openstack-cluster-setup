5418245fe6646c7da2275a8f86455a4c6006e5bb
Andy Bavier <acb@cs.princeton.edu>
2015-04-01 15:40:58 -0400

    Add missing script

---
+++ files/install-ovx.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+apt-get update
+apt-get install git maven mongodb software-properties-common -y
+add-apt-repository ppa:webupd8team/java -y
+apt-get update
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get install oracle-java7-installer oracle-java7-set-default -y
+
+cd /home/ubuntu; su -c "git clone https://github.com/OPENNETWORKINGLAB/OpenVirteX.git -b 0.0-MAINT" ubuntu
+#sh OpenVirteX/scripts/ovx.sh
