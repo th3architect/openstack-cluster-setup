aeb1d8d05a3f92e21c286b253a6e74c04ebba6e9
Andy Bavier <acb@cs.princeton.edu>
2015-05-20 15:40:33 -0400

    Subnets for WAN and LAN networks

--- scripts/network-setup.sh
+++ scripts/network-setup.sh
@@ -16,10 +16,21 @@ function create-subnet {
     CIDR=$2
     GW=$3
 
-    neutron subnet-show nat-net 2>&1 > /dev/null
+    neutron subnet-show $NAME-net 2>&1 > /dev/null
     if [ "$?" -ne 0 ]
     then
 	neutron subnet-create $NAME-net --name $NAME-net $CIDR --gateway=$GW --enable-dhcp=false
+    fi
+}
+
+function create-subnet-no-gateway {
+    NAME=$1
+    CIDR=$2
+
+    neutron subnet-show $NAME-net 2>&1 > /dev/null
+    if [ "$?" -ne 0 ]
+    then
+	neutron subnet-create $NAME-net --name $NAME-net $CIDR --no-gateway --enable-dhcp=false
     fi
 }
 
@@ -27,8 +38,11 @@ create-flat-net nat
 create-subnet nat 172.16.0.0/16 172.16.0.1
 
 create-flat-net ext
+
 create-flat-net wan
+# Can't use 10/8 for this because XOS already uses it for private networks
+create-subnet-no-gateway wan 192.168.128.0/17
+
 create-flat-net lan
-
-
-
+# The IP range for this is bogus, Neutron just needs something
+create-subnet-no-gateway lan 12.0.0.0/8
